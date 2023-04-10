0b8f01d07dfe527631df72a8a90a325b4a4dd986
QuanNA <quanna@relipasoft.com>
2023-04-10 10:32:31 +0700

    commit

--- helpers/axios.helper.js
+++ helpers/axios.helper.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import { RPC_URL } from '@root/helpers/constants'
 
+const nftApiEndpoint = 'https://nft.api.infura.io'
+const Auth = Buffer.from(`${process.env.INFURA_API_KEY}:${process.env.INFURA_API_KEY_SECRET}`).toString('base64')
+
 export const callInfuraApi = async (chain, method, filter) => {
   const params = {
     jsonrpc: '2.0',
@@ -15,4 +18,12 @@ export const callInfuraApi = async (chain, method, filter) => {
   }
   console.log(response.data)
   return response.data.result
+}
+
+export const getAllCollectionsOfUser = async (chainId, walletAddress) => {
+  const endpoint = `${nftApiEndpoint}/networks/${chainId}/accounts/${walletAddress}/assets/nfts?cursor=eyJhbGciOiJIUzI1NiJ9.ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpkWE4wYjIxUVlYSmhiWE1pT25zaWQyRnNiR1YwUVdSa2NtVnpjeUk2SWpCNE56QmxNV0U0TnpNME16TmlOR1V5WmpCbVlUbGxaV1ZsT0RRNU9XVTVZek0zTldJd05UTTBZU0o5TENKclpYbHpJanBiSWpFMk5UUTNOVEU1TlRZdU5ERTNJbDBzSW5kb1pYSmxJanA3SW05M2JtVnlYMjltSWpvaU1IZzNNR1V4WVRnM016UXpNMkkwWlRKbU1HWmhPV1ZsWldVNE5EazVaVGxqTXpjMVlqQTFNelJoSW4wc0lteHBiV2wwSWpveE1EQXNJbTltWm5ObGRDSTZNQ3dpYjNKa1pYSWlPbHRkTENKa2FYTmhZbXhsWDNSdmRHRnNJanBtWVd4elpTd2lkRzkwWVd3aU9qRXdOeXdpY0dGblpTSTZNU3dpZEdGcGJFOW1abk5sZENJNk1Td2lhV0YwSWpveE5qZ3hNRGszTURNM2ZRLjBIU0p5aGRudkw2OU1tbV8tR0NoOVVuWTRJaWtvdWVSUWVXUENoX3Y2Y1U.MOjbL6lXVZa6DeRdzQhA_xTuMF6TUMrrkzMyYAwNkX0`
+  const response = await axios.get(endpoint, {
+    headers: { Authorization: `Basic ${Auth}` },
+  })
+  return response
 }
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import {
   verifySwapTransactionOnUniswap,
   verifyAddLiquidityTransactionOnUniswap,
 } from '@root/services/uniswap-verifier.service'
-import { ethers } from 'ethers'
 
 const app = express()
 const port = 5000
@@ -25,31 +24,14 @@ const main = async () => {
     '500'
   )
   console.log('Is swap transaction: ', isSwap)
-  // const isAddLiq = await verifyAddLiquidityTransactionOnUniswap(
-  //   CHAIN.POLYGON,
-  //   "0x064b1b2668b181a0f8837c8676e1337348c306d9",
-  //   CURRENCY_INFO.POLYGON.WBTC.address,
-  //   CURRENCY_INFO.POLYGON.WETH.address,
-  //   "0.00066215"
-  // );
-  // console.log("Is add liquidity transaction: ", isAddLiq);
+  const isAddLiq = await verifyAddLiquidityTransactionOnUniswap(
+    CHAIN.POLYGON,
+    '0x064b1b2668b181a0f8837c8676e1337348c306d9',
+    CURRENCY_INFO.POLYGON.WBTC.address,
+    CURRENCY_INFO.POLYGON.WETH.address,
+    '0.00066215'
+  )
+  console.log('Is add liquidity transaction: ', isAddLiq)
 }
 
-// main()
-
-// const provider = new ethers.providers.JsonRpcProvider(
-//   process.env.ETHEREUM_RPC_URL,
-// )
-// provider
-//   .getTransaction(
-//     '0xe95c1be02d8f733d1ac09a57c6cccfcf514a778d980dc64267788837b04e1f6e',
-//   )
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err))
-
-// provider
-//   .getBalance(
-//     '0x9595eeAF82907cE461842E533427336d27484A0A',
-//     '0x23adc20af86187428a4088318d00487001a4e31c5efa0e40896d6ccfde4970e6',
-//   )
-//   .then((res) => console.log(res))
+main()
